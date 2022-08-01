47d221b1134cd40c13051ed754ad7507ee2b415d
Joseph Harold <josephharold.timbang.dev@gmail.com>
2022-08-01 12:12:15 +0800

    added sidebar, backToTop

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,24 @@
 const navUl = document.getElementById('navUl');
 const dropDownButton = document.getElementById('dropDownButton');
-
+const backToTopButton = document.getElementById('backToTopButton');
 const toggleNavUl = ()=>{
-	navUl.classList.toggle('h-0');
+	navUl.classList.toggle('dropdown-active');
+	console.log('asdfasd');
+}
+
+const closeNavUl = (e)=>{
+	const isOutsideNavUl  = !navUl.contains(e.target);
+	const isOutsideDropDownButt = !dropDownButton.contains(e.target);
+	if(isOutsideDropDownButt && isOutsideNavUl){
+		navUl.classList.remove('dropdown-active');
+	}
+}
+
+const backToTop = ()=>{
+	window.scrollTo({top: 0, behavior: "smooth"});
 }
 
 
-
-
 dropDownButton.addEventListener('click', ()=>toggleNavUl());
-
-
+window.addEventListener('click', (e)=>closeNavUl(e));
+backToTopButton.addEventListener('click',()=>{backToTop()});
